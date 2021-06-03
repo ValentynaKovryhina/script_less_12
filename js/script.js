dcae3b8c4c9380541fa27dd978858f9d539d9014
Valentyna Kovryhina <valentynakovryhina@gmail.com>
2021-06-03 15:22:34 +0300

    add ganres, add show only for privat:false

--- js/script.js
+++ js/script.js
@@ -1,28 +1,14 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-if (numberOfFilms < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (numberOfFilms > 30) {
-    alert("Вы киноман");
-} else {
-    alert("Вы классический зритель");
-};
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели? Введите число!', '');
+    }
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -32,23 +18,62 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 1; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
 
-    let lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
+        let lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
 
-    while (!lastFilmName || lastFilmName.length < 1 || lastFilmName.length > 50) {
-        lastFilmName =
-            prompt('Один из последних просмотренных фильмов? Введите название фильма от 1 до 50 символов', '');
+        while (!lastFilmName || lastFilmName.length < 1 || lastFilmName.length > 50) {
+            lastFilmName =
+                prompt('Один из последних просмотренных фильмов? Введите название фильма от 1 до 50 символов', '');
+        }
+
+        let lastFilmRating = prompt('На сколько оцените его?', '');
+
+        while (!lastFilmRating || lastFilmRating.length < 1 || lastFilmRating.length > 50) {
+            lastFilmRating =
+                prompt('На сколько оцените его? Введите оценку от 1 до 50 символов', '');
+        }
+
+        personalMovieDB.movies[lastFilmName] = lastFilmRating;
+    };
+}
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (numberOfFilms > 30) {
+        alert("Вы киноман");
+    } else {
+        alert("Вы классический зритель");
+    };
+}
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
     }
+}
 
-    let lastFilmRating = prompt('На сколько оцените его?', '');
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        let yourGenres = prompt(`Ваш любимый жанр под номером ${i + 1}`, '');
 
-    while (!lastFilmRating || lastFilmRating.length < 1 || lastFilmRating.length > 50) {
-        lastFilmRating =
-            prompt('На сколько оцените его? Введите оценку от 1 до 50 символов', '');
+        while (!yourGenres || yourGenres.length < 1 || yourGenres.length > 50) {
+            yourGenres =
+                prompt(`Ваш любимый жанр под номером ${i + 1} Введите название от 1 до 50 символов`, '');
+        }
+
+        personalMovieDB.genres[i] = yourGenres;
     }
+}
 
-    personalMovieDB.movies[lastFilmName] = lastFilmRating;
-};
+start();
 
-console.log(personalMovieDB);
+rememberMyFilms();
+
+writeYourGenres();
+
+detectPersonalLevel();
+
+showMyDB();
